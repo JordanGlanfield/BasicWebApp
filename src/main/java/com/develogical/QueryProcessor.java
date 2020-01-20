fbeb2f0ee20de9b461b13e32396287ae4a851725
Jordan Glanfield <j.glanfield@hotmail.co.uk>
2020-01-20 10:52:59 +0000

    Added another query keyword.

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -2,11 +2,16 @@ package com.develogical;
 
 public class QueryProcessor {
 
-    public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare")) {
+    public String process(String rawQuery) {
+        String query = rawQuery.toLowerCase();
+
+        if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
+        } else if (query.contains("banteronium")) {
+            return "the playful and friendly exchange of teasing remarks.\n" +
+                    "\"there was much good-natured banter\"";
         }
         return "";
     }

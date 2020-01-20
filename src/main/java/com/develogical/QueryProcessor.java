edafe19a6ddf67ca23b036b17f0a5d5b874baf0b
Jordan Glanfield <j.glanfield@hotmail.co.uk>
2020-01-20 10:55:53 +0000

    Fixed query processor 'banter' response.

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -9,7 +9,7 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("banteronium")) {
+        } else if (query.contains("banter")) {
             return "the playful and friendly exchange of teasing remarks.\n" +
                     "\"there was much good-natured banter\"";
         }

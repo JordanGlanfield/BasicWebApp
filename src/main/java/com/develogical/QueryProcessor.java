895ba0e9510e3988d8f5e544ce0a1b928b65b911
Harry Davis <ib.harrydavis@gmail.com>
2020-01-21 11:08:48 +0000

    Test commit

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -5,7 +5,7 @@ public class QueryProcessor {
     public String process(String rawQuery) {
         String query = rawQuery.toLowerCase();
 
-        if (query.contains("shakespeare")) {
+        if (query.contains("shakespearee")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";

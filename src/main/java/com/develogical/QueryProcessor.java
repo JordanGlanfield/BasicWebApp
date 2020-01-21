69e02b4fb88dcbe608c51deb360422977389abe2
Jordan Glanfield <j.glanfield@hotmail.co.uk>
2020-01-21 11:14:31 +0000

    Added team name query to query processor.

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -9,9 +9,11 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("bantere")) {
+        } else if (query.contains("banter")) {
             return "the playful and friendly exchange of teasing remarks.\n" +
                     "\"there was much good-natured banter\"";
+        } else if (query.contains("what is your team name")) {
+            return "Our team name is banterwagon";
         }
         return "";
     }
